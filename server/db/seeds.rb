0fb3fb0043fbb1a95828655cdbf31757022dda03
Xihai Luo <xihai.luo01@gmail.com>
2022-01-15 05:58:42 +0000

    add faker gem and setup sqlite3 db

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require('faker')
+
+3.times do
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password)
+end
