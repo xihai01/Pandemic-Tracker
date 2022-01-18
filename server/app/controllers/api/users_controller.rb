f6e1b9502042dcb80923671f06cde8259c3db5a1
simplyDonald <donaldabuah@gmail.com>
2022-01-18 17:19:30 +0000

    bug fix for authorization

--- server/app/controllers/api/users_controller.rb
+++ server/app/controllers/api/users_controller.rb
@@ -1,13 +1,17 @@
 class Api::UsersController < ApplicationController
+  before_action :require_login
+
   def index
-    if session
-      users = User.all
-      render json: users
-    else 
-      render json: { message: "Unauthorized user: Permission denied" }
-    end
+    users = User.all
+    render json: users
+    
   end
 
+  private
 
+  def require_login
+    # do stuff to check if user is logged in
+    render json: { message: "Unauthorized user: Permission denied" } unless session[:user_id]
+  end
 
 end
