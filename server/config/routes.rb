56987d43948d817e4ab4eeb8a64eb90574e38981
Xihai Luo <86758440+xihai01@users.noreply.github.com>
2022-01-19 14:44:38 -0500

    update routes.rb
    delete users and admin route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -14,10 +14,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   namespace :api do
-    resources :users
     resources :maps, only: [:index] # fetches geoJSON
     resources :map_color, only: [:index] # fetches stage colors of regions
-    resources :admin
   end
 
   # You can have the root of your site routed with "root"
