1a385bb8524aa4d7107b994074dc87dbd079d7f7
Xihai Luo <xihai.luo01@gmail.com>
2022-01-20 00:47:48 -0500

    move health region click handler into a helper file

--- client/src/components/HealthRegion.js
+++ client/src/components/HealthRegion.js
@@ -1,19 +1,6 @@
 import "./HealthRegion.css";
-import axios from "axios";
+import { handleClick } from "../helpers/handleClick";
 import classNames from "classnames";
-
-const handleClick = function (setRestriction, phuID) {
-  // get restrictions data from api
-  axios
-    .get(`http://localhost:3000/api/restrictions/${phuID}`)
-    .then((data) => {
-      // set state variable to render a container with restriction data
-      setRestriction(data);
-    })
-    .catch((error) => {
-      console.log("error: ", error);
-    });
-};
 
 /**
  *
