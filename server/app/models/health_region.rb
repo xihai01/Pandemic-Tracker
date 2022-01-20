1e1a02ae19931340baf5d1e6020c245ed09c62f1
Xihai Luo <86758440+xihai01@users.noreply.github.com>
2022-01-19 23:14:39 -0500

    update health_region.rb
    delete "t.strings" as they were a mistake

--- server/app/models/health_region.rb
+++ server/app/models/health_region.rb
@@ -1,11 +1,5 @@
 class HealthRegion < ActiveRecord::Base
   belongs_to :stage
-
-  t.string   "region_name"
-  t.string   "region_code"
-
   validates :region_name, presence: true
   validates :region_code, presence: true
- 
-
 end
