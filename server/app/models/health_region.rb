558ab7baa5af9c8264068fbfc46c86b32156527e
simplyDonald <donaldabuah@gmail.com>
2022-01-20 03:11:16 +0000

    add validations for  models

--- server/app/models/health_region.rb
+++ server/app/models/health_region.rb
@@ -1,3 +1,11 @@
 class HealthRegion < ActiveRecord::Base
   belongs_to :stage
+
+  t.string   "region_name"
+  t.string   "region_code"
+
+  validates :region_name, presence: true
+  validates :region_code, presence: true
+ 
+
 end
