76c724a4f9184400b7f1eb8344dd897404fba92f
Xihai Luo <xihai.luo01@gmail.com>
2022-01-22 00:11:28 -0500

    clean up code

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -14,52 +14,56 @@ export default function Map(props) {
 
         // create overlay
         const overlay = new google.maps.OverlayView();
+        let layer;
         overlay.onAdd = function () {
           const pane = d3.select("map");
-          const layer = d3
+          layer = d3
             .select(overlay.getPanes().overlayLayer)
             .append("div")
             .attr("class", "SvgOverlay");
+        };
+        overlay.draw = function () {
+          layer.select("svg").remove();
+          const markerOverlay = overlay;
+          const overlayProjection = markerOverlay.getProjection();
 
-
-          overlay.draw = function () {
-            layer.select("svg").remove();
-            const markerOverlay = overlay;
-            const overlayProjection = markerOverlay.getProjection();
-
-            // Turn the overlay projection into a d3 projection
-            const transform = d3.geoTransform({point: function(x, y) {
+          // Turn the overlay projection into a d3 projection
+          const transform = d3.geoTransform({
+            point: function (x, y) {
               let d = new google.maps.LatLng(y, x);
               d = overlayProjection.fromLatLngToDivPixel(d);
               this.stream.point(d.x + 4000, d.y + 4000);
-            }
+            },
           });
+          // listen for drag and zoom events
 
-            //const proj = d3.geoTransform(transform);
-            // const proj = d3.geoMercator();
-            const path = d3.geoPath().projection(transform);
-            const svg = layer.append("svg");
-            const adminDivisions = svg
+          const path = d3.geoPath().projection(transform);
+          const svg = layer.append("svg");
+          const adminDivisions = svg
             .append("g")
             .attr("class", "AdminDivisions");
-            adminDivisions
-              .selectAll("path")
-              .data(props.mapData.features)
-              .enter()
-              .append("path")
-              .attr("d", path) // update existing paths
+          adminDivisions
+            .selectAll("path")
+            .data(props.mapData.features)
+            .enter()
+            .append("path")
+            .attr("d", path); // update existing paths
 
-          };
+          // only re-render region when map is still
         };
+        console.log(props.mapData.features);
+
+        //const proj = d3.geoTransform(transform);
+        // const proj = d3.geoMercator();
+
         if (!props.loading) {
-        overlay.setMap(map);
+          overlay.setMap(map);
         }
       })
       .catch((error) => {
         console.log("error loading map", error);
       });
   }, [props.mapData]);
-  console.log(props.mapData.features);
 
   return <p>this is a map from google</p>;
 }
