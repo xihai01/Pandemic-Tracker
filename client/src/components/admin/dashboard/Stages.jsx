15406ad67e61ddbd7e3ed0072c861d1547915f10
simplyDonald <donaldabuah@gmail.com>
2022-01-24 23:01:37 +0000

    add test table to stages component

--- client/src/components/admin/dashboard/Stages.jsx
+++ client/src/components/admin/dashboard/Stages.jsx
@@ -1,9 +1,7 @@
-import axios from "axios";
-import { useState, useEffect } from 'react';
-import { Box, Checkbox, TableRow, TableCell, TableHead, TableSortLabel } from '@mui/material';
 import AdminBoard from "./AdminBoard";
 import useStagesData from "hooks/useStagesData";
 import adminReducer, { SET_STAGES } from "reducer/admin_reducer";
+import MaterialTable from "material-table";
 
 
 function Stages() {
@@ -12,17 +10,30 @@ function Stages() {
   const {state,data,columns,editRow,deleteRow}= useStagesData();
   
   
+const testColumn = [
+  { title: 'Name', field: 'name' },
+  { title: 'Surname', field: 'surname', initialEditValue: 'initial edit value' },
+  { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+  {
+    title: 'Birth Place',
+    field: 'birthCity',
+    lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+  },
+]
+
+const testData = [
+  { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+  { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+]
 
 
-
-  console.log(state);
-  console.log(`TABLEDATA`,data);
+  console.log(`TABLEDATANOW`,data);
 
 
   return ( 
     <AdminBoard>
       <h2>THIS IS THE STAGE COMPONENT FOR STAGES TABLE</h2>
-
+      <MaterialTable title="Lockdown Stages" columns={testColumn} data={testData} />;
 
     </AdminBoard>
     
