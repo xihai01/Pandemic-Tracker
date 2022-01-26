5e354e51439d4b4dfaf884b84611bab1c52b0ac4
simplyDonald <donaldabuah@gmail.com>
2022-01-26 00:40:19 +0000

    restyle admin  login page

--- client/src/components/admin/AdminLogin.js
+++ client/src/components/admin/AdminLogin.js
@@ -10,7 +10,7 @@ import {
   Link,
 } from "@material-ui/core";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { Typography } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 
@@ -48,71 +48,90 @@ export default function AdminLogin() {
 
   const paperStyle = {
     padding: 20,
-    height: "70vh",
+    height: "50vh",
     width: 280,
-    margin: "20px auto",
+    margin: "100px auto",
   };
   const avatarStyle = { backgroundColor: "#00AB55" };
   const btnStyle = { 
     margin: "8px 0",
     backgroundColor: "#005249" };
 
+    const Item = styled(Paper)(({ theme }) => ({
+      ...theme.typography.h3,
+      padding: theme.spacing(1),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    }));
+
   return (
-    <Grid>
-      <Paper elevation={10} style={paperStyle}>
-        <Grid align="center">
-          <Avatar style={avatarStyle}>
-            <LockOutlinedIcon style={{ border: "none" }} />
-          </Avatar>
-          <h2>Admin</h2>
-          {state.error && (
-            <Alert severity="error">
-              <AlertTitle>Error</AlertTitle>
-              <strong>Invalid email or password</strong>
-            </Alert>
-          )}
-        </Grid>
-        <form onSubmit={validate}>
-          <TextField
-            fullWidth
-            required
-            type="email"
-            name="email"
-            placeholder="Enter Email address"
-            label="Email"
-            value={state.email}
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            required
-            name="password"
-            type="password"
-            placeholder="Enter valid password"
-            label="Password"
-            value={state.password}
-            onChange={handleChange}
-          />
-          <Button
-            type="submit"
-            onClick={validate}
-            color="primary"
-            variant="contained"
-            fullWidth
-            style={btnStyle}
-          >
-            SIGN IN
-          </Button>
-        </form>
-        <Typography>
-          <Link href="#">Forgot Password</Link>
-        </Typography>
-        <Typography>
-          {" "}
-          Create new User?
-          <Link href="#">Sign Up</Link>
-        </Typography>
-      </Paper>
+    <Grid container spacing={2} alignItems="center"  justifyContent="center">
+      <Grid item xs={7}>
+        <Paper elevation={0}>
+        <Box
+          component="img"
+          src="images/undraw_server_down_s-4-lk.svg"
+        />
+        </Paper>
+      </Grid>
+      <Grid item>
+        <Paper elevation={4} style={paperStyle}>
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <LockOutlinedIcon style={{ border: "none" }} />
+            </Avatar>
+            <Typography variant="h4">Admin</Typography>
+            {state.error && (
+              <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                <strong>Invalid email or password</strong>
+              </Alert>
+            )}
+          </Grid>
+          <form onSubmit={validate}>
+            <TextField
+              fullWidth
+              required
+              type="email"
+              name="email"
+              placeholder="Enter Email address"
+              label="Email"
+              value={state.email}
+              onChange={handleChange}
+            />
+            <TextField
+              fullWidth
+              required
+              name="password"
+              type="password"
+              placeholder="Enter valid password"
+              label="Password"
+              value={state.password}
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              onClick={validate}
+              color="primary"
+              variant="contained"
+              fullWidth
+              style={btnStyle}
+            >
+              SIGN IN
+            </Button>
+          </form>
+          <Typography>
+            <Link href="#">Forgot Password?</Link>
+          </Typography>
+          <Typography>
+            {" "}
+            Create new User
+            <Link href="#"> Sign Up</Link>
+          </Typography>
+        </Paper>
+      </Grid>
+      
+      
     </Grid>
   );
 }
